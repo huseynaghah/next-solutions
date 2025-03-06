1106b3cb64629a566973bfed807a4fe3d5b89dcb
huseynaghah <huseynaghanh@code.edu.az>
2025-03-06 04:21:35 +0400

    fix(main): trackbar renew

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import RootLayoutComponent from '@/core/layouts/root/root.layout';
 import {AntdRegistry} from '@ant-design/nextjs-registry';
 import '@/assets/styles/style.scss';
 import './page.style.scss';
+import Head from 'next/head';
 
 
 export const metadata: Metadata = {
     title: 'Next Solution',
     description: 'İdeyalarınızı Rəqəmsal Həllərə Çeviririk!',
+    icons: 'favicon.ico',
 };
 
 export default function Layout({
@@ -19,6 +21,10 @@ export default function Layout({
     return (
         <html lang='en'>
         <body className={'layout'}>
+        <Head>
+            <link rel='icon' type='favicon' href='/favicon.ico' />
+        </Head>
+        <link rel='icon' href='./favicon.ico' sizes='any'/>
         <AntdRegistry>
             <RootLayoutComponent>
                 {children}
